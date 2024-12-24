7a29771bb9b821db9c26ea0005b2c20ec134df41
saich-cendyn <csai@cendyn.com>
2024-12-24 08:45:00 +0530

    fix: mobile menu toggle

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('main-menu');
+    const menuClose = document.getElementById('main-menu-close');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuToggle.addEventListener('click', () => {
+      mobileMenu.classList.remove('hidden');
+    });
+
+    menuClose.addEventListener('click', () => {
+      mobileMenu.classList.add('hidden');
+    });
+  });
